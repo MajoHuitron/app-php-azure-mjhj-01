a525bcdf056a86f85bee6541b8df7f84b6abaef4
Majo Huitrón <116222516+MajoHuitron@users.noreply.github.com>
2022-10-21 10:28:32 -0500

    Actualización visual
    corrección

--- select.php
+++ select.php
@@ -1,12 +1,27 @@
 <?php
-  include ('connect.php');  
-  session_start();
+  include ('connect.php');
   
-  $sql = "SELECT Continent FROM world.country WHERE Name = '" . $_SESSION['pais'] . "'"; 
+  $sql = "SELECT * FROM country"; 
   $query = $conn -> prepare($sql); 
   $query -> execute(); 
   $results = $query -> fetchAll(PDO::FETCH_OBJ); 
 
-//  var_dump($results); // 
-  echo $results[0] -> Continent;
+  if($query -> rowCount() > 0)   { 
+    echo "<table border='1px'>";
+    echo "<tr>
+      <td>Name</td>
+      <td>Continent</td>
+      <td>Region</td>
+      <td>Population</td>
+    </tr>";
+    foreach($results as $result) { 
+      echo "<tr>
+        <td>" . $result->Name . "</td>
+        <td>" . $result->Continent . "</td>
+        <td>" . $result->Region . "</td>
+        <td>" . $result->Population . "</td>
+      </tr>";
+    }
+    echo "</table>";
+  }
 ?>
